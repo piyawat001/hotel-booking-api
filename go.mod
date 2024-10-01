ea11add5d5703eed3d3d7a08b0592e31156e1ea0
Piyawat Phimsri <piyawat.ph@inet.co.th>
2024-10-01 16:49:25 +0700

    core frontend

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.22.6
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
+	github.com/gorilla/handlers v1.5.2 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -17,15 +18,18 @@ import (
 var client *mongo.Client
 
 func main() {
+	// Load environment variables
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found")
 	}
 
+	// MongoDB URI from environment variables
 	uri := os.Getenv("MONGODB_URI")
 	if uri == "" {
 		log.Fatal("You must set your 'MONGODB_URI' environmental variable.")
 	}
 
+	// Connect to MongoDB
 	var err error
 	client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
 	if err != nil {
@@ -37,12 +41,19 @@ func main() {
 		}
 	}()
 
+	// Create the router
 	router := mux.NewRouter()
 
 	// Add routes
 	routes.AuthRoutes(router, client)
 	routes.BookingRoutes(router, client)
 
+	// Enable CORS for frontend access from port 3000
+	corsOptions := handlers.AllowedOrigins([]string{"http://localhost:3000"})
+	headersOk := handlers.AllowedHeaders([]string{"Authorization", "Content-Type"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"})
+
+	// Start server with CORS middleware
 	log.Println("Server is running on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(corsOptions, headersOk, methodsOk)(router)))
 }
